8b9627aa22229a813c03493875028ad24c8bbab4
Carlos Philippe <carl2678@yahoo.fr>
2022-08-24 12:52:33 -0400

    modified file of project microverse

--- project/js/index.js
+++ project/js/index.js
@@ -1,28 +1,90 @@
 // !! IMPORTANT README:
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place.
 
 /***********
 INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
+  - Select the project you would
+    like to complete from the dropdown
     menu.
   - Click the "RUN TESTS" button to
-    run the tests against the blank 
+    run the tests against the blank
     pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
+  - Click the "TESTS" button to see
+    the individual test cases.
     (should all be failing at first)
   - Start coding! As you fulfill each
-    test case, you will see them go   
+    test case, you will see them go
     from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
+  - As you start to build out your
+    project, when tests are failing,
+    you should get helpful errors
     along the way!
     ************/
 
 // PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
 
-// Once you have read the above messages, you can delete all comments. 
+// Once you have read the above messages, you can delete all comments.
 "use strict";
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+  let stepA = Math.abs(x - z);
+  let stepB = Math.abs(y - z);
+
+  if(stepA - stepB){
+    return (stepA < stepB) ? "Cat A": "Cat B";
+  }
+  return "Mouse C";
+}
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const xyz = readLine().split(' ');
+
+        const x = parseInt(xyz[0], 10);
+
+        const y = parseInt(xyz[1], 10);
+
+        const z = parseInt(xyz[2], 10);
+
+        let result = catAndMouse(x, y, z);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
